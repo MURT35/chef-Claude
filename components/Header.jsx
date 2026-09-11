17d2c739431925bb498ac3d4eaf1f0afb38d18e9
MURT35 <murtadaothman8@gmail.com>
2026-09-12 01:46:45 +0200

    Initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+      <header>
+ <img className="logo-img" src="../images/chef-claude-icon.png" alt="Chef Claude logo" />
+      <h1 className="logo-text" >Chef Claude</h1>
+      </header>
+      
+    )
+}
