8e376414f571dda54a4a760e2e123d0ba680a99d
Murtada Othman <murtadaothman8@gmail.com>
2026-09-26 14:08:04 +0200

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,12 +4,7 @@ import IngredientsList from "./IngredientsList"
 import { getRecipeFromIngredients } from "../ai"
 
 export default function Main() {
-    const [ingredients, setIngredients] = React.useState([
-        "all the main spices",
-        "pasta",
-        "ground beef",
-        "tomato paste",
-    ])
+    const [ingredients, setIngredients] = React.useState([])
     const [recipe, setRecipe] = React.useState("")
     const [error, setError] = React.useState("")
     const [isLoading, setIsLoading] = React.useState(false)
